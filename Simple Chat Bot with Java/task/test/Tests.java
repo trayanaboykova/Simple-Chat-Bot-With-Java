e1a78b9b1c37b34a02feba1e6cebfb3710131bae
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-05 20:18:03 +0200

    adding files

--- Simple Chat Bot with Java/task/test/Tests.java
+++ Simple Chat Bot with Java/task/test/Tests.java
@@ -1,90 +1,40 @@
-import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
+import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.TestCase;
 
 import java.util.List;
 
 
-class Clue {
-    int age;
-    String name;
-    int count;
-
-    Clue(String name, int age, int count) {
-        this.age = age;
-        this.name = name;
-        this.count = count;
-    }
-}
-
-
-public class Tests extends StageTest<Clue> {
+public class Tests extends StageTest {
 
     @Override
-    public List<TestCase<Clue>> generate() {
+    public List<TestCase> generate() {
         return List.of(
-            new TestCase<Clue>()
-                .setInput("Marry\n1\n0\n5\n10")
-                .setAttach(new Clue("Marry", 40, 10))
+            new TestCase<>()
         );
     }
 
     @Override
-    public CheckResult check(String reply, Clue clue) {
+    public CheckResult check(String reply, Object clue) {
 
         String[] lines = reply.trim().split("\n");
 
-        int length = 9 + clue.count + 1;
-
-        if (lines.length != length) {
+        if (lines.length != 2) {
             return CheckResult.wrong(
-                "You should output " + length + " lines " +
-                    "(for the count number " + clue.count +").\n" +
-                    "Lines found: " + lines.length + "\n" +
-                    "Your output:\n" +
-                    reply
+                "You should output exactly 2 lines!\n" +
+                    "Lines found: " + lines.length
             );
         }
 
-        String lineWithName = lines[3].toLowerCase();
-        String name = clue.name.toLowerCase();
+        String secondLine = lines[1];
 
-        if (!lineWithName.contains(name)) {
+        if (!secondLine.matches(".*\\d.*")) {
             return CheckResult.wrong(
-                "The name was " + clue.name + "\n" +
-                    "But the 4-th line was:\n" +
-                    "\"" + lines[3] + "\"\n\n" +
-                    "4-th line should contain a name of the user"
+                "The second line should contain a year!\n" +
+                    "Your second line: \"" + secondLine + "\""
             );
-        }
-
-        String lineWithAge = lines[6].toLowerCase();
-        String age = Integer.toString(clue.age);
-
-        if (!lineWithAge.contains(age)) {
-            return CheckResult.wrong(
-                "Can't find a correct age " +
-                    "in the last line of output! " +
-                    "Maybe you calculated the age wrong?\n\n" +
-                    "Your last line: \n" + "\"" + lines[6] + "\""
-            );
-        }
-
-        for (int i = 0; i < clue.count + 1; i++) {
-            String numLine = lines[i + 8];
-            String actualNum = i + "!";
-
-            if (!numLine.equals(actualNum)) {
-                return CheckResult.wrong(
-                    "Expected " + (i+8) + "-th line: \n" +
-                        "\"" + actualNum + "\"\n" +
-                        "Your "+ (i+8) + "-th line: \n" +
-                        "\"" + numLine + "\""
-                );
-            }
         }
 
         return CheckResult.correct();
     }
-
 }
